8260a45753007b218239a2a85e698aed9dd2cff7
John Doe <johndoe@example.com>
2023-03-18 09:26:47 +0100

    primera pagina, y pasar a la siguiente

---
+++ script.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById('myButton');
+const page2 = document.getElementById('page2');
+myButton.addEventListener('click', function() {
+  document.body.innerHTML = 'dsfsdf';
+  page2.style.display = 'block';
+});
