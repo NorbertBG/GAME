17caeb3a052a40c4d2bdebcd8b7c3dfc4b4e7f62
John Doe <johndoe@example.com>
2023-03-19 20:00:14 +0100

    primera idea de estructura de preguntas

--- js/script-data.js
+++ js/script-data.js
@@ -1,26 +1,26 @@
 const gameStory = [
     {
-        title: 'Pagina 1',
-        subtitle: "Choose an option:",
+        title: 'Congratulations, you have found a new job starting on Monday, to celebrate...',
+        subtitle: "Make a choice:",
         background: "url('css/images/page-1.png')",
         options: [
             {
-                option: "Hola",
+                option: "Go partying and go directly to work without any sleep",
                 Score: 10,
             },
             {
-                option: "hola2",
-                Score: 50,
+                option: "Go on a trip and arrive the same day",
+                Score: 30,
             },
             {
-                option: "hola3",
+                option: "Go for lunch in the afternoon with friends",
                 Score: 90,
             }
         ]
     },
     {
-        title: 'Pagina 2',
-        subtitle: "Choose an option:",
+        title: 'You already have a first project assigned...',
+        subtitle: "Make a choice:",
         background: "url('css/images/Test-image.jpeg')",
         options: [
             {
@@ -38,9 +38,47 @@ const gameStory = [
         ]
     },
     {
-        title: 'Pagina 3',
-        subtitle: "Choose an option:",
+        title: 'You have a misunderstanding with a colleague...',
+        subtitle: "Make a choice:",
+        background: "url('css/images/page-1.png')",
+        options: [
+            {
+                option: "Hola",
+                Score: 10,
+            },
+            {
+                option: "hola2",
+                Score: 50,
+            },
+            {
+                option: "hola3",
+                Score: 90,
+            }
+        ]
+    },
+    {
+        title: 'You are invited to your first afterwork...',
+        subtitle: "Make a choice:",
         background: "url('css/images/Test-image.jpeg')",
+        options: [
+            {
+                option: "Hola",
+                Score: 10,
+            },
+            {
+                option: "hola2",
+                Score: 50,
+            },
+            {
+                option: "hola3",
+                Score: 90,
+            }
+        ]
+    },
+    {
+        title: 'Your boss calls you to go to his office...',
+        subtitle: "Make a choice:",
+        background: "url('css/images/page-1.png')",
         options: [
             {
                 option: "Hola",
